b4454f9a5466fc720b1641010cad18d059e08e5d
maximedasilva <maximedasilva40@gmail.com>
2019-05-31 17:25:21 +0200

    chore(env) met le endpoint n1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
+
 const app = express();
 
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+app.use(cors());
+
+app.get('/', (req, res) => {
+  const object = {};
+  object.first = 'Hello World!';
+  object.second = process.env.MY_SECRET;
+  res.send(object);
+});
+
+app.listen(process.env.port, () =>
+  console.log(`Example app listening on port 3000!`),
 );

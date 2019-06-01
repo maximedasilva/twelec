863eda00f0246013199bdb4168f6a4dd9a2bb00c
maximedasilva <maximedasilva40@gmail.com>
2019-06-01 23:33:44 +0200

    feat: ajoute le module twitter pour récupérer les tweets

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import Twitter from 'twitter'
 
 const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const object = {};
-  object.first = 'Hello World!';
+  object.first = "Hello World!";
   res.send(object);
 });
 
-app.listen(process.env.port, () =>
-  console.log(`Example app listening on port 3000!`),
-);
+app.listen(process.env.port, () => {
+  var twitter = new Twitter({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_key,
+    access_token_secret: process.env.access_secret
+  });
+  var stream = twitter.stream('statuses/filter', {track: 'javascript'});
+  stream.on('data', function(event) {
+    console.log(event && event.text);
+  });
+  console.log(`Example app listening on port 3000!`);
+});
